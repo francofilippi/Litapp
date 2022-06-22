a1ad42c8e016c0fcd953a02e128ab6df611df93f
francofilippi <francodanielfilippi@gmail.com>
2022-06-22 14:40:32 -0300

    searchBar optimiz2

--- front-end/src/Components/SearchProduct/index.js
+++ front-end/src/Components/SearchProduct/index.js
@@ -9,7 +9,6 @@ import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import ClickAwayListener from '@mui/material/ClickAwayListener';
 
 // Componentes
 import SearchProductError from './SearchProductError';
@@ -29,70 +28,68 @@ export default function SearchProduct({
 
   return (
 
-    <ClickAwayListener >
-      <Autocomplete
-        sx={{ width: '100%', maxWidth: '650px' }}
-        disableCloseOnSelect={disableCloseOnSelect}
-        renderTags={() => null}
-        multiple={multiple}
-        limitTags={limitTags}
-        filterSelectedOptions={filterSelectedOptions}
-        value={value}
-        onChange={(event, newValue) => setValue(newValue)}
-        open={open}
-        onOpen={() => {
-          setOpen(true);
-        }}
-        onClose={() => {
-          setOpen(false);
-        }}
-        isOptionEqualToValue={(option, value) => option.productName === value.productName}
-        getOptionLabel={(option) => option.productName}
-        options={searchOptions}
-        loadingText='Cargando..'
-        renderOption={
-          searchOptions.length === 1 ? () => <SearchProductError /> :
-            (props, option) => (
-              <ListItem
-                variant='listItemSearchProduct'
-                key={option.productName}
-                {...props}>
-                <Box maxHeight='150px' width='100%'>
-                  <Stack
-                    direction="row"
-                    width="100%"
-                    alignItems="center"
-                    justifyContent="space-between"
-                  >
-                    <Typography variant='body2' sx={{ 'maxWidth': '280px' }}>{option.productName}</Typography>
-                    <Paper variant='imgListPaper' sx={{ display: 'flex', width: '6rem', height: '6rem' }}>
-                      <img
-                        src={option.image}
-                        style={{ 'aspectRatio': '1/1', 'borderRadius': '10%' }}
-                        alt=""
-                      />
-                    </Paper>
-                  </Stack>
-                </Box>
-              </ ListItem>
-            )
-        }
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            placeholder='Busca un producto..'
-            InputProps={{
-              ...params.InputProps,
-              endAdornment: (
-                <>
-                  {loadingSearchOptions ? <CircularProgress color="inherit" size={20} /> : null}
-                  {params.InputProps.endAdornment}
-                </>
-              ),
-            }}
-          />
-        )}
-      />
-    </ClickAwayListener>
+    <Autocomplete
+      sx={{ width: '100%', maxWidth: '650px' }}
+      disableCloseOnSelect={disableCloseOnSelect}
+      renderTags={() => null}
+      multiple={multiple}
+      limitTags={limitTags}
+      filterSelectedOptions={filterSelectedOptions}
+      value={value}
+      onChange={(event, newValue) => setValue(newValue)}
+      open={open}
+      onOpen={() => {
+        setOpen(true);
+      }}
+      onClose={() => {
+        setOpen(false);
+      }}
+      isOptionEqualToValue={(option, value) => option.productName === value.productName}
+      getOptionLabel={(option) => option.productName}
+      options={searchOptions}
+      loadingText='Cargando..'
+      renderOption={
+        searchOptions.length === 1 ? () => <SearchProductError /> :
+          (props, option) => (
+            <ListItem
+              variant='listItemSearchProduct'
+              key={option.productName}
+              {...props}>
+              <Box maxHeight='150px' width='100%'>
+                <Stack
+                  direction="row"
+                  width="100%"
+                  alignItems="center"
+                  justifyContent="space-between"
+                >
+                  <Typography variant='body2' sx={{ 'maxWidth': '280px' }}>{option.productName}</Typography>
+                  <Paper variant='imgListPaper' sx={{ display: 'flex', width: '6rem', height: '6rem' }}>
+                    <img
+                      src={option.image}
+                      style={{ 'aspectRatio': '1/1', 'borderRadius': '10%' }}
+                      alt=""
+                    />
+                  </Paper>
+                </Stack>
+              </Box>
+            </ ListItem>
+          )
+      }
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          placeholder='Busca un producto..'
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <>
+                {loadingSearchOptions ? <CircularProgress color="inherit" size={20} /> : null}
+                {params.InputProps.endAdornment}
+              </>
+            ),
+          }}
+        />
+      )}
+    />
   );
 }
